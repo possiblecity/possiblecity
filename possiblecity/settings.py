328083b0676432d0984ee349af890e6f5138e650
Douglas Meehan <dmeehan@gmail.com>
2012-06-10 12:20:05 -0400

    upgrading Pinax

--- possiblecity/settings.py
+++ possiblecity/settings.py
@@ -2,13 +2,14 @@
 # Default Django settings for possiblecity
 
 import os.path
-import posixpath
 
-PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+PACKAGE_ROOT = os.path.abspath(os.path.dirname(__file__))
+
 
 ADMINS = (
     ('Douglas Meehan', 'dmeehan@gmail.com'),
-    )
+)
 
 MANAGERS = ADMINS
 
@@ -21,13 +22,24 @@ SITE_ID = 1
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
-DEBUG_TOOLBAR_CONFIG = {
-    "INTERCEPT_REDIRECTS": False,
-    }
-
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+
+#==============================================================================
+# Databases
+#==============================================================================
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3", # Add "postgresql_psycopg2", "postgresql", "mysql", "sqlite3" or "oracle".
+        "NAME": "dev.db",                       # Or path to database file if using sqlite3.
+        "USER": "",                             # Not used with sqlite3.
+        "PASSWORD": "",                         # Not used with sqlite3.
+        "HOST": "",                             # Set to empty string for localhost. Not used with sqlite3.
+        "PORT": "",                             # Set to empty string for default. Not used with sqlite3.
+    }
+}
 
 #==============================================================================
 # Localization
@@ -37,14 +49,13 @@ TIME_ZONE = 'America/New_York'
 LANGUAGE_CODE = 'en-us'
 USE_I18N = True    #internationalization machinery
 USE_L10N = True    #format dates, numbers and calendars according to locale
-
+# If you set this to False, Django will not use timezone-aware datetimes.
+USE_TZ = True
 
 #==============================================================================
 # Project URLS and media settings
 #==============================================================================
 
-# tells Pinax to serve media through the staticfiles app.
-SERVE_MEDIA = DEBUG
 
 ROOT_URLCONF = 'possiblecity.urls'
 
@@ -54,30 +65,21 @@ MEDIA_URL = "/media/uploads/"
 STATIC_ROOT = os.path.join(PROJECT_ROOT, "media", "static")
 STATIC_URL = "/media/static/"
 
-ADMIN_MEDIA_PREFIX = posixpath.join(STATIC_URL, "admin/")
-
 STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, "static"),
+    os.path.join(PACKAGE_ROOT, "static"),
 ]
 
 STATICFILES_FINDERS = [
-    "staticfiles.finders.FileSystemFinder",
-    "staticfiles.finders.AppDirectoriesFinder",
-    "staticfiles.finders.LegacyAppDirectoriesFinder",
-    "compressor.finders.CompressorFinder",
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
-
-# django-compressor is turned off by default due to deployment overhead for
-# most users.
-COMPRESS = False
-COMPRESS_OUTPUT_DIR = "cache"
 
 #==============================================================================
 # Templates
 #==============================================================================
 
 TEMPLATE_DIRS = [
-    os.path.join(PROJECT_ROOT, "templates"),
+    os.path.join(PACKAGE_ROOT, "templates"),
 ]
 
 TEMPLATE_CONTEXT_PROCESSORS = [
@@ -85,24 +87,18 @@ TEMPLATE_CONTEXT_PROCESSORS = [
     "django.core.context_processors.debug",
     "django.core.context_processors.i18n",
     "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.core.context_processors.tz",
     "django.core.context_processors.request",
     "django.contrib.messages.context_processors.messages",
-
-    "staticfiles.context_processors.static",
-
-    "pinax.core.context_processors.pinax_settings",
-
-    "pinax.apps.account.context_processors.account",
-
-    "notification.context_processors.notification",
-    "announcements.context_processors.site_wide_announcements",
-    "social_auth.context_processors.social_auth_by_type_backends",
+    "pinax_utils.context_processors.settings",
+    "account.context_processors.account",
 ]
 
 
 TEMPLATE_LOADERS = [
-    "django.template.loaders.filesystem.load_template_source",
-    "django.template.loaders.app_directories.load_template_source",
+    "django.template.loaders.app_directories.Loader",
+    "django.template.loaders.filesystem.Loader",
 ]
 
 #==============================================================================
@@ -115,12 +111,7 @@ MIDDLEWARE_CLASSES = [
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django_openid.consumer.SessionConsumer",
     "django.contrib.messages.middleware.MessageMiddleware",
-    "pinax.apps.account.middleware.LocaleMiddleware",
-    "pagination.middleware.PaginationMiddleware",
-    "pinax.middleware.security.HideSensistiveFieldsMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 #==============================================================================
@@ -142,50 +133,20 @@ MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 # Authentication
 #==============================================================================
 
-AUTHENTICATION_BACKENDS = (
-    "social_auth.backends.twitter.TwitterBackend",
-    "social_auth.backends.facebook.FacebookBackend",
-    "social_auth.backends.google.GoogleOAuthBackend",
-    "social_auth.backends.google.GoogleOAuth2Backend",
-    "social_auth.backends.google.GoogleBackend",
-    #'social_auth.backends.yahoo.YahooBackend',
-    #'social_auth.backends.contrib.linkedin.LinkedinBackend',
-    #'social_auth.backends.contrib.livejournal.LiveJournalBackend',
-    #'social_auth.backends.contrib.orkut.OrkutBackend',
-    #'social_auth.backends.contrib.foursquare.FoursquareBackend',
-    #'social_auth.backends.contrib.github.GithubBackend',
-    #'social_auth.backends.OpenIDBackend',
-    #'django.contrib.auth.backends.ModelBackend',
-    'pinax.apps.account.auth_backends.AuthenticationBackend',
-    )
-
-# django-social-auth
-SOCIAL_AUTH_ENABLED_BACKENDS = ('facebook', 'twitter', 'google-oauth2')
-SOCIAL_AUTH_LOGIN_REDIRECT_URL = '/'
-SOCIAL_AUTH_NEW_USER_REDIRECT_URL = '/about/what_next'
-# SOCIAL_AUTH_NEW_ASSOCIATION_REDIRECT_URL = '/new-association-redirect-url/'
-# SOCIAL_AUTH_DISCONNECT_REDIRECT_URL = '/account-disconnected-redirect-url/'
-# SOCIAL_AUTH_ERROR_KEY = 'social_errors'
-SOCIAL_AUTH_DEFAULT_USERNAME = 'new_social_auth_user'
-SOCIAL_AUTH_EXPIRATION = 'expires'
-SOCIAL_AUTH_COMPLETE_URL_NAME  = 'socialauth_complete'
-SOCIAL_AUTH_ASSOCIATE_URL_NAME = 'socialauth_associate_complete'
-SOCIAL_AUTH_ASSOCIATE_BY_MAIL = True
-FACEBOOK_EXTENDED_PERMISSIONS = ['email']
 
 # Account
-ACCOUNT_OPEN_SIGNUP = False
+ACCOUNT_OPEN_SIGNUP = True
 ACCOUNT_USE_OPENID = False
-ACCOUNT_REQUIRED_EMAIL = True
+ACCOUNT_REQUIRED_EMAIL = False
 ACCOUNT_EMAIL_VERIFICATION = False
 ACCOUNT_EMAIL_AUTHENTICATION = False
 ACCOUNT_UNIQUE_EMAIL = EMAIL_CONFIRMATION_UNIQUE_EMAIL = False
 
-ABSOLUTE_URL_OVERRIDES = {
-    "auth.user": lambda o: "/profiles/profile/%s/" % o.username,
-    }
+#ABSOLUTE_URL_OVERRIDES = {
+#    "auth.user": lambda o: "/profiles/profile/%s/" % o.username,
+#    }
 
-AUTH_PROFILE_MODULE = "profiles.Profile"
+#AUTH_PROFILE_MODULE = "profiles.Profile"
 
 LOGIN_URL = "/account/login/" # @@@ any way this can be a url name?
 LOGIN_REDIRECT_URLNAME = "what_next"
@@ -198,56 +159,43 @@ LOGOUT_REDIRECT_URLNAME = "home"
 
 INSTALLED_APPS = (
     # django
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.messages',
-    'django.contrib.humanize',
-    #'django.contrib.gis',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.sites",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    #"django.contrib.gis",
 
-    # Third party Pinax apps
-    'pinax.templatetags',
-    'pinax.apps.account',
-    'pinax.apps.signup_codes',
-
-    # Pinax theme
-    'pinax_theme_bootstrap',
+    # theme
+    "pinax_theme_bootstrap_account",
+    "pinax_theme_bootstrap",
+    "django_forms_bootstrap",
 
     # third party apps
-    'notification',
-    'staticfiles',
-    'compressor',
-    'debug_toolbar',
-    'mailer',
-    'timezones',
-    'metron', # analytics and metrics
-    'announcements',
-    'pagination',
-    'idios',
-    'emailconfirmation',
+    "account",
+    "timezones",
+    "metron",
 
-    'haystack', # search
-    'south', # database migrations
-    'oembed',
-    'imagekit',
-    'django_markup', # required for blog
-    'taggit', # required for blog, float, & lotxlot
-    'social_auth', # registration via social networks
-    'django_generic_flatblocks',
-
+    #"haystack", # search
+    #"south", # database migrations
+    #"oembed",
+    #"imagekit",
+    #"django_markup", # required for blog
+    #"taggit", # required for blog, float, & lotxlot
+    #"social_auth", # registration via social networks
 
     # backbeat apps
-    'inlines',
-    'twittools',
-    'images',
-    'text',
+    #'inlines',
+    #'twittools',
+    #'images',
+    #'text',
 
     # local apps
-    'blog',
-    'about',
-    'profiles',
+    #'blog',
+    #'about',
+    #'profiles',
     #'float',
     #'lotxlot',
 
@@ -264,21 +212,27 @@ INSTALLED_APPS = (
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler'
+    "version": 1,
+    "disable_existing_loggers": False,
+    "filters": {
+        "require_debug_false": {
+            "()": "django.utils.log.RequireDebugFalse"
         }
     },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
+    "handlers": {
+        "mail_admins": {
+            "level": "ERROR",
+            "filters": ["require_debug_false"],
+            "class": "django.utils.log.AdminEmailHandler"
+        }
+    },
+    "loggers": {
+        "django.request": {
+            "handlers": ["mail_admins"],
+            "level": "ERROR",
+            "propagate": True,
+            },
+        }
 }
 
 
@@ -286,8 +240,7 @@ LOGGING = {
 # Email
 #==============================================================================
 
-# django-mailer
-EMAIL_BACKEND = "mailer.backend.DbBackend"
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 EMAIL_CONFIRMATION_DAYS = 2
 EMAIL_DEBUG = DEBUG
 
--- possiblecity/urls.py
+++ possiblecity/urls.py
@@ -2,45 +2,38 @@
 
 from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls.static import static
 from django.views.generic import TemplateView, RedirectView
-from django.views.generic.simple import direct_to_template
 
 from django.contrib import admin
 admin.autodiscover()
 
-from pinax.apps.account.openid_consumer import PinaxConsumer
-
-handler500 = "pinax.views.server_error"
 
 urlpatterns = patterns('',
     # admin
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r"^$", RedirectView.as_view(url="/blog"), {
-        "template": "homepage.html",
-    }, name="home"),
-    url(r"^admin/invite_user/$", "pinax.apps.signup_codes.views.admin_invite_user",
-        name="admin_invite_user"),
+    url(r"^$", TemplateView.as_view(template_name="homepage.html"),name="home"),
+
+
+
     url(r"^admin/", include(admin.site.urls)),
-    url(r"^about/", include("about.urls")),
-    url(r"^account/", include("pinax.apps.account.urls")),
-    url(r"^openid/", include(PinaxConsumer().urls)),
-    url(r"^profiles/", include("idios.urls")),
-    url(r"^notices/", include("notification.urls")),
-    url(r"^announcements/", include("announcements.urls")),
-    url(r'', include('social_auth.urls')),
+
+
+    url(r"^account/", include("account.urls")),
+
+
+
+
 
     # blog
-    (r'^blog/', include('blog.urls')),
+    #(r'^blog/', include('blog.urls')),
 
 
     # search
-    (r'^search/', include('haystack.urls')),
+    #(r'^search/', include('haystack.urls')),
 
 )
 
-if settings.SERVE_MEDIA:
-    urlpatterns += patterns("",
-        url(r"", include("staticfiles.urls")),
-    )
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
