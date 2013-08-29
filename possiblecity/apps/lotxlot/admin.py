16b443bcfde3fa32ac7dda63c2008b96ff663e21
Douglas Meehan <dmeehan@gmail.com>
2013-08-29 20:52:33 +0000

    added idea inlines for lot

--- possiblecity/apps/lotxlot/admin.py
+++ possiblecity/apps/lotxlot/admin.py
@@ -2,12 +2,19 @@
 
 from django.contrib import admin
 
+from apps.ideas.models import Idea
+
 from .models import Lot
+
+class IdeaInline(admin.TabularInline):
+    model = Idea
 
 class LotAdmin(admin.ModelAdmin):
 	list_display = ('id', 'address', 'city', 'state', 'is_vacant', 'is_public', 'is_visible',)
 	list_editable = ('is_visible',)
 	search_fields = ['address',]
 	list_filter = ('city',)
+        
+        inlines = [ IdeaInline, ]
 
 admin.site.register(Lot, LotAdmin)
