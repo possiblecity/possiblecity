da0d707d0e63e08ffd36002ac06e63480abb22b2
Douglas Meehan <dmeehan@gmail.com>
2013-09-10 17:01:44 +0000

    email fixes

--- possiblecity/settings/production.py
+++ possiblecity/settings/production.py
@@ -8,7 +8,7 @@ from base import *
 # Site
 #==============================================================================
 # See: https://docs.djangoproject.com/en/1.5/releases/1.5/#allowed-hosts-required-in-production
-ALLOWED_HOSTS = ['possiblecity.co', 'www.possiblecity.co', 'thepossiblecity.com', 'www.thepossiblecity.com']
+ALLOWED_HOSTS = ['possiblecity.co', 'www.possiblecity.co', 'smtp.webfaction.com']
 
 
 #==============================================================================
@@ -18,22 +18,22 @@ ALLOWED_HOSTS = ['possiblecity.co', 'www.possiblecity.co', 'thepossiblecity.com'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#email-host
-EMAIL_HOST = environ.get('EMAIL_HOST', 'smtp.webfaction.com')
+EMAIL_HOST = environ.get('DJANGO_EMAIL_HOST', 'smtp.webfaction.com')
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#email-host-password
-EMAIL_HOST_PASSWORD = environ.get('DJANGO_EMAIL_PASSWORD', '')
+EMAIL_HOST_PASSWORD = environ.get('DJANGO_EMAIL_HOST_PASSWORD', '')
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#email-host-user
-EMAIL_HOST_USER = environ.get('EMAIL_HOST_USER', 'possiblecity')
+EMAIL_HOST_USER = environ.get('DJANGO_EMAIL_HOST_USER', 'possiblecity')
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#email-port
-#EMAIL_PORT = environ.get('EMAIL_PORT', 587)
+EMAIL_PORT = environ.get('DJANGO_EMAIL_PORT', '587')
+
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-port
+EMAIL_USE_TLS = environ.get('DJANGO_EMAIL_USE_TLS', 'True')
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#email-subject-prefix
 EMAIL_SUBJECT_PREFIX = '[%s] ' % SITE_NAME
-
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-use-tls
-#EMAIL_USE_TLS = True
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = 'info@possiblecity.co'
