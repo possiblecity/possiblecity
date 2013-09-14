aa281082cae13578bf79668beea0f372005bdae7
Douglas Meehan <dmeehan@gmail.com>
2013-09-14 16:43:53 -0400

    responsive cleanup

--- possiblecity/apps/lotxlot/serializers.py
+++ possiblecity/apps/lotxlot/serializers.py
@@ -6,11 +6,13 @@ from rest_framework_gis import serializers as geo_serializers
 
 class LotSerializer(geo_serializers.GeoFeatureModelSerializer):
 	idea_set = serializers.HyperlinkedRelatedField(many=True, view_name="ideas_idea_detail")
+	idea_count = serializers.Field(source='idea_set.count')
+	size = serializers.Field(source='get_sqft')
 	
 	class Meta:
 		model = Lot
 		geo_field = 'bounds'
-		fields = ('id', 'slug', 'address', 'is_public', 'idea_set')
+		fields = ('id', 'slug', 'address', 'is_public', 'idea_set', 'size', 'idea_count')
 
 class LotPointSerializer(geo_serializers.GeoFeatureModelSerializer):
 	class Meta:
