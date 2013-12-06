1b7d3bd73ae58f0e8036992710f8d888abde7fea
Douglas Meehan <dmeehan@gmail.com>
2013-12-06 06:39:45 +0000

    added an admin.py

---
+++ possiblecity/apps/comments/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from .models import Comment
+
+
+class CommentAdmin(admin.ModelAdmin):  
+    list_display = ["user", "content_type", "is_public"]
+    list_filter = ["is_public", "content_type"]
+
+
+admin.site.register(Comment, CommentAdmin)
