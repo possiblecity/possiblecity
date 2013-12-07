f2b45c95a6fbb9eea8e65e55dd9042e18c779ff4
Douglas Meehan <dmeehan@gmail.com>
2013-12-07 17:29:29 +0000

    added urls.py

---
+++ possiblecity/apps/ideas/urls.py
@@ -0,0 +1,21 @@
+# ideas/urls.py
+
+from django.conf.urls import *
+from django.views.generic import TemplateView
+
+from .views.explore import IdeaDetailView, IdeaListView, IdeaListFeaturedView
+from .views.share import IdeaCreateView, IdeaUpdateView
+
+urlpatterns = patterns('',
+    # user input of ideas
+    url(r'^add/$',
+        IdeaCreateView.as_view(),
+        name='ideas_idea_create'),
+    url(r'^/update/(?P<pk>[\w-]+)$',
+        IdeaUpdateView.as_view(),
+        name='ideas_idea_update'),
+
+    url(r'^$', IdeaListView.as_view(), name='ideas_idea_list'),
+    url(r'^(?P<slug>[-\w]+)/$', IdeaDetailView.as_view(), name = 'ideas_idea_detail'),
+
+)
