15e04c8d5cb1ccd661375886c45d47726154e303
Douglas Meehan <dmeehan@gmail.com>
2013-12-14 23:34:11 -0500

    tweaked css styles

--- possiblecity/apps/lotxlot/autocomplete_light_registry.py
+++ possiblecity/apps/lotxlot/autocomplete_light_registry.py
@@ -3,4 +3,8 @@ import autocomplete_light
 
 from .models import Lot
 
-autocomplete_light.register(Lot,search_fields=['^address',])
+autocomplete_light.register(Lot,
+	search_fields=['^address',], 
+	autocomplete_js_attributes={
+        'placeholder': 'Enter the address of a lot.',
+    })
