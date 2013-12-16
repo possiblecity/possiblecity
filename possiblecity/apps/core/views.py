a5721ba7fd2cccf918dc40ee61c65e2752154707
Douglas Meehan <dmeehan@gmail.com>
2013-12-16 01:30:16 -0500

    homepage updates

--- possiblecity/apps/core/views.py
+++ possiblecity/apps/core/views.py
@@ -1,20 +1,19 @@
 # possiblecity/views.py 
 
-from django.views.generic.list import ListView
+from django.views.generic import TemplateView
 
 from actstream.models import Action
 
 from apps.ideas.models import Idea
 
-class HomepageView(ListView):
-    queryset = Idea.objects.filter(featured=True)
-    context_object = "featured_ideas"
+class HomepageView(TemplateView):
     template_name = "homepage.html"
 
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get a context
         context = super(HomepageView, self).get_context_data(**kwargs)
         extra_context = {
+            'featured_ideas': Idea.objects.filter(featured=True)[:3],
             'idea': Idea.objects.filter(featured=True).order_by('?')[0],
             'activity_stream': Action.objects.filter(public=True)[:8]
         }
