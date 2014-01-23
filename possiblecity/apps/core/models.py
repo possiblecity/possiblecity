ba555c99f7172b7c41d14b6bded911c78acf2f28
Douglas Meehan <dmeehan@gmail.com>
2014-01-23 15:22:33 +0000

    workon on errors from robots

--- possiblecity/apps/core/models.py
+++ possiblecity/apps/core/models.py
@@ -1,7 +1,7 @@
 # core/models.py
 from django.db.models.signals import post_save
 
-from actstream.signals import action
+from actstream import action
 from actstream.models import Follow
 from phileo.signals import object_liked, object_unliked
 from notification import models as notification
@@ -16,17 +16,16 @@ def unliked_action(sender, request, object=None, target=None, **kwargs):
     action.send(request.user, verb=u'unfavorited', target=object)
 
 def notify_following(sender, instance, **kwargs):
-	follower = instance.user
-	target = instance.follow_object
-	if hasattr(target, 'user'):
-            notify_list = [ target.user,]
-        elif hasattr(target, 'first_name'):
-            notify_list = [ target ]
-        else:
-            return
-     
-        notification.send( notify_list, "new_follower", 
-           { "follower": follower, "target": target })	
+    follower = instance.user
+    target = instance.follow_object
+    if hasattr(target, 'user'):
+        notify_list = [ target.user,]
+    elif hasattr(target, 'first_name'):
+        notify_list = [ target, ]
+    else:
+        return
+    notification.send( notify_list, "new_follower", 
+        { "follower": follower, "target": target })	
 
 object_liked.connect(liked_action)
 
